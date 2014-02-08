f32fc7c9d50ffe32570ce7b0349a02267895a1f8
bizi <alexandr@bizikov.ru>
2014-02-09 00:11:50 +0600

    добавлен выбор поисковиков при поиске

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,31 @@
 // Поиск по сайту
-function customSearchInGoogle() {
-    var input = document.getElementById('searchInGoogle');
-    if (input.value !== '') {
-        window.open("http://google.com/search?q=" + "site:bizikov.ru%20" + input.value)
+function customSearch() {
+    var searchString = document.getElementById('search');
+    var searchEngine = document.getElementById('searchEngine').value;
+    var yandex = 'http://yandex.ru/yandsearch?site=http://bizikov.ru&text=';
+    var mailru = 'http://go.mail.ru/search?site=bizikov.ru&q='
+    var google = 'http://google.com/search?q=site:bizikov.ru%20';
+
+    function searchResult(link, value) {
+        window.open(link + value);
+    }
+
+    if (searchString.value !== '') {
+        switch (searchEngine) {
+            case 'yandex':
+                searchResult(yandex, searchString.value);
+                break;
+            case 'mailru':
+                searchResult(mailru, searchString.value);
+                break;
+            case 'google':
+            default :
+                searchResult(google, searchString.value);
+                break;
+        }
+
     } else {
-        input.placeholder = "Для поиска введи запрос";
+        searchString.placeholder = "Для поиска введи запрос";
         return false;
     }
 }
