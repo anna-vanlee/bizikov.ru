2be15ec647406093894f4168072ff9cdf3f3e9d8
bizi <alexandr@bizikov.ru>
2014-09-14 21:28:32 +0600

    Обновлена пагинация

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,11 +37,9 @@ function NavigateThrough(event) {
     switch (event.keyCode ? event.keyCode : event.which ? event.which : null) {
         case 0x25:
             link = document.getElementById('previous');
-            console.log("Нажал на левую стрелку");
             break;
         case 0x27:
             link = document.getElementById('next');
-            console.log("Нажал на правую стрелку");
             break;
     }
     if (link && link.href) document.location = link.href;
