fc708af1ceff13d3817466d72ff12869dbedd6ec
Alexandr Bizikov <alexandr@bizikov.ru>
2015-01-11 23:10:16 +0500

    Добавлена кнопка с комментариями

---
+++ assets/js/disqus.js
@@ -0,0 +1,9 @@
+var btn = document.getElementById('showBtn');
+document.addEventListener('click', function () {
+    btn.style.display = 'none';
+    var dsq = document.createElement('script');
+    dsq.type = 'text/javascript';
+    dsq.async = true;
+    dsq.src = '//' + 'bizikov.disqus.com/embed.js';
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+});
