06e91c1c0e35fdc68dc0fb23fd92fcf385b3e9b2
Alexandr Bizikov <alexandr@bizikov.ru>
2015-01-16 19:28:11 +0500

    bug fix

--- assets/js/disqus.js
+++ assets/js/disqus.js
@@ -1,9 +1,11 @@
 var btn = document.getElementById('showBtn');
-btn.addEventListener('click', function () {
-    btn.style.display = 'none';
-    var dsq = document.createElement('script');
-    dsq.type = 'text/javascript';
-    dsq.async = true;
-    dsq.src = '//' + 'bizikov.disqus.com/embed.js';
-    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-});
+if (btn) {
+    btn.addEventListener('click', function () {
+        btn.style.display = 'none';
+        var dsq = document.createElement('script');
+        dsq.type = 'text/javascript';
+        dsq.async = true;
+        dsq.src = '//' + 'bizikov.disqus.com/embed.js';
+        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+    });
+}
