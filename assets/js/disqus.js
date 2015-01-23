09c0f86f021ef30b910c4428a9359ca0ad0fd55e
Alexandr Bizikov <alexandr@bizikov.ru>
2015-01-24 00:11:46 +0500

    Обновлен показ комментариев

--- assets/js/disqus.js
+++ assets/js/disqus.js
@@ -1,11 +1,17 @@
 var btn = document.getElementById('showBtn');
 if (btn) {
     btn.addEventListener('click', function () {
-        btn.style.display = 'none';
-        var dsq = document.createElement('script');
-        dsq.type = 'text/javascript';
-        dsq.async = true;
-        dsq.src = '//' + 'bizikov.disqus.com/embed.js';
-        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+        var disqus = document.getElementById('dsq-2');
+        if (!disqus) {
+            btn.text = 'Скрыть комментарии';
+            var dsq = document.createElement('script');
+            dsq.type = 'text/javascript';
+            dsq.async = true;
+            dsq.src = '//' + 'bizikov.disqus.com/embed.js';
+            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+        } else {
+            btn.text = 'Оставить комментарий';
+            document.getElementById('disqus_thread').innerHTML = '';
+        }
     });
 }
